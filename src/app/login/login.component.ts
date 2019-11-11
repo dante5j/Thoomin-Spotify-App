73ee1ef48bf754ed41ff4646fa5047cf7afc8993
Charlie Levine <charlielevine@Charlies-MacBook-Pro.local>
2019-11-11 18:03:52 -0500

    Login service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  constructor(private loginService: LoginService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  login() {
+    this.loginService.login();
   }
-
 }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  url = 'https://thoomin-spotify-app.firebaseapp.com';
+  constructor(private http: HttpClient) {}
+
+  login() {
+    return this.http.get(`${this.url}/api/user/login`);
+  }
+}
