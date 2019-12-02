4971e69b4419625c987fc1a07d47915ab6ee37c2
dante5j <32501485+dante5j@users.noreply.github.com>
2019-12-02 00:01:46 -0500

    began queue implementation

--- src/app/home/queue/queue.component.ts
+++ src/app/home/queue/queue.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
+import { Track } from 'src/app/models/spotify.model';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-queue',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./queue.component.scss']
 })
 export class QueueComponent implements OnInit {
+  tracks: Track[];
+  id: string;
+  name: string;
 
-  constructor() { }
+  constructor(
+    private spotifyService: SpotifyService,
+    private route: ActivatedRoute
+  ) { 
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params.id;
+      this.getPlaylist();
+    });
+  }
+
+  getPlaylist() {
+    this.spotifyService.getPlaylist(this.id).subscribe((res: any) => {
+      this.name = res.name;
+      this.tracks = [];
+      res.tracks.items.forEach(item => {
+        this.tracks.push(item.track);
+      });
+    });
   }
 
 }
